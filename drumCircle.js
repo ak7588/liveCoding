f0f00c6109b6d4462414b5960cd247c7ff58ec21
Amina Kobenova <56433807+ak7588@users.noreply.github.com>
2022-11-06 12:57:54 +0000

    Create drumCircle.js

---
+++ drumCircle.js
@@ -0,0 +1,36 @@
+p5 = new P5()
+s0.init({src: p5.canvas})
+src(s0).out()
+p5.hide();
+
+p5.createCanvas(600, 600);
+p5.colorMode(p5.HSB, 360, 100, 100, 100);
+p5.noStroke();
+p5.fill(192, 100, 64, 60);
+
+p5.draw = ()=>{
+  p5.clear();
+  p5.background(0);
+  p5.ellipse();
+}
+render(o0)
+
+noise(0.1).add(osc(1,2,5).modulateScale(osc(5)), [-1,1].smooth().fast(0.5),.5).color(3.5, 3.0, 3.0).scale(0.05).blend(o0, 0.7).brightness(-0.5).out()
+
+shape(2).modulateScroll
+
+noise(0.1).add(osc(1,2,5).modulateScale(osc(5)), [-1,1].smooth().fast(0.5),.5).color(3.5, 3.0, 3.0).modulateScale(o3).out(o3)
+
+render()
+
+////really nice!!!!!
+voronoi(10,1,5).brightness(()=>Math.random()*0.15)
+  .modulatePixelate(noise(0.5,0.5),100)
+  .color(10,0.5,10.4).contrast(-0.5)
+  .out(o3)
+
+render(o1)
+
+solid(1).out(o2)
+
+hush()
